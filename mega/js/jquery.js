57fe60a94f3f616bc273d6bca821073aa28de371
araa78 <ubeom78@gmail.com>
2024-06-02 18:45:33 +0900

    Add files via upload

---
+++ mega/js/jquery.js
@@ -0,0 +1,8 @@
+        $(function(){
+            $('.swiper_slide').slick({
+                autoplay: true,
+                autoplaySpeed: 5000,
+                dots:true,
+                infinite:true,
+            });
+        });
